4ce76c2a2a71c2ed62f0112e4a5eb22bc5250298
Hamza Nazir <hamza@trimulabs.com>
2020-12-22 16:04:24 +0500

    Hook file added

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+
+
+    const searchApi = async  (initialSearchVal) =>{ //async initialSearchVal =>{
+       try
+       {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: initialSearchVal,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses)
+    }
+    catch (err){
+       setErrorMessage('ohhh')
+    }
+    }
+
+    useEffect( () =>{ // Runs only first time if second argument is empty list
+        searchApi('pasta')
+    }, [])
+
+return [searchApi, results,errorMessage]
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar  from "../Components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
 
-    const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-
-
-    const searchApi = async() =>{
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'san jose'
-            }
-        });
-        setResults(response.data.businesses)
-    }
+    const [term, setTerm] = useState('');
+   const [searchApi, results, errorMessage] = useResults();
     return (
         <View>
             <SearchBar
                 term = {term}
-                onTermChange = {newTerm => setTerm(newTerm)}
-                onTermSubmit = {searchApi}
+                onTermChange = {val =>setTerm(val)}  // also onTermChange = {setTerm}
+                onTermSubmit = {()=>searchApi(term)}
                 
             />
             <Text>Results found are  {results.length}</Text>
-            <Text></Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null }
         </View>
     )
 }
